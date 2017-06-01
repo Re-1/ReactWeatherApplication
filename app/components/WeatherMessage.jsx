3ce39a1a967683d07f57da7fc92ad42741800ef5
Re-1 <noirriel@gmail.com>
2017-06-01 13:14:15 +0200

    minor change

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -2,7 +2,9 @@ var React = require('react');
 
 var WeatherMessage = ({temp, location}) => {
   return (
+    <div>
       <h1 className="text-center">It is {temp} degrees Celsius in {location}.</h1>
+    </div>  
   )
 };
 
